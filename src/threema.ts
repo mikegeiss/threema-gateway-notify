d7fdf7550ec94c8ec2534dfd066cc24cbc45d5fa
Michael Geiss <michael.geiss@europace.de>
2019-10-10 20:14:30 +0200

    added docs to ThreemaConnector constructor and changed signature of constructor

--- src/threema.ts
+++ src/threema.ts
@@ -1,32 +1,24 @@
-import assert from 'assert';
 import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'querystring';
 
 export class ThreemaConnector {
-  toIds: string[];
-  fromId: string;
-  apiSecret: string;
 
-  // todo nicht mit context sondern konkreten params arbeiten
-  // todo assertions nicht hier machen?
-  constructor(context: any) {
-    assert.ok(context.secrets.THREEMA_TO_IDS, 'Parameter "THREEMA_TO_IDS" wurde nicht gesetzt.');
-    assert.ok(context.secrets.THREEMA_FROM_ID, 'Parameter "THREEMA_FROM_ID" wurde nicht gesetzt.');
-    assert.ok(context.secrets.THREEMA_API_SECRET, 'Parameter "THREEMA_API_SECRET" wurde nicht gesetzt.');
-    this.toIds = context.secrets.THREEMA_TO_IDS.split(',');
-    this.fromId = context.secrets.THREEMA_FROM_ID;
-    this.apiSecret = context.secrets.THREEMA_API_SECRET;
+  /**
+   * @param {string[]} toIds - List of Threema-Ids receiving the notification
+   * @param {string} fromId - ThreemaId sending the notification
+   * @param {string} apiSecret - Threema Gateway Api Secret corresponds to the sender
+   */
+  constructor(private toIds: string[], private fromId: string, private apiSecret: string) {
   }
 
   async sendMessage(message: string) {
-    console.log('send to Threema');
     this.toIds.forEach(id => this.sendMessageToId(message, id));
   }
 
   private sendMessageToId(message: string, toId: string) {
     const data = {
       from: this.fromId,
-      to: this.toIds,
+      to: toId,
       text: message,
       secret: this.apiSecret
     };
